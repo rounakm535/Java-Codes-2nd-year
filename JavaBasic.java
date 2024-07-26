75f212fd643941680fa319917279ddf7b2de7a04
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-07-26 13:09:10 +0530

    Add files via upload

---
+++ JavaBasic.java
@@ -0,0 +1,22 @@
+public class JavaBasic {
+
+    public static void Printing() {
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+    }
+    public static void main(String args[]) {
+        int a = 100;
+        int b = 300;
+
+        //swap
+        int temp  = a ;
+        a = b;
+        b = temp;
+
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+        
+    }
+}
